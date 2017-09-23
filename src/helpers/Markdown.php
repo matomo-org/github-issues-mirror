93b41b2a581e4011211da6848d1956543c7d9175
Lukas Winkler <l.winkler23@mailbox.org>
2017-09-23 13:12:58 +0200

    links to other issues

--- src/helpers/Markdown.php
+++ src/helpers/Markdown.php
@@ -20,6 +20,7 @@ class Markdown extends \Parsedown
      */
     public function text($markdown) {
         $markdown = $this->parseMentions($markdown);
+        $markdown = $this->parseIssueMentions($markdown);
         $this->setBreaksEnabled(true);
         $html = parent::text($markdown);
 
@@ -31,6 +32,12 @@ class Markdown extends \Parsedown
         $regex = '/\@(\w+)/';
         return preg_replace($regex, "<a class='mention' href='https://github.com/$1'>$0</a>", $markdown);
     }
+
+    private function parseIssueMentions($markdown) {
+        $regex = '/#(\d+)/';
+        return preg_replace($regex, "<a href='/$1'>$0</a>", $markdown);
+    }
+
 
     /**
      * <a href="http://issues.piwik.org/attachments/1199/swelen_dateslider.swf">swelen_dateslider.swf</a>
